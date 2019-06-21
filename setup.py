03822c80dd6444b879e4a1da8ac6acee6e76c488
Emil Bækdahl <emilsbaekdahl@gmail.com>
2019-06-21 22:55:36 +0200

    Release

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='sentiment_lexicon',
     version='0.1.0',
+    packages=['sentiment_lexicon'],
     url='https://github.com/emilbaekdahl/sentiment_lexicon',
     author='Emil Bækdahl',
     author_email="emilsbaekdahl@gmail.com",
-    packages=find_packages()
+    project_urls={'Source Code': 'https://github.com/emilbaekdahl/sentiment_lexicon',
+                  'Documentation': 'https://emilbaekdahl.github.io/sentiment_lexicon/'}
 )
