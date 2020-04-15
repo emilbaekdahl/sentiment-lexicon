69bb9ed74d6cadb71ed0145f5d78e97d5ddad7c9
Emil Bækdahl <emilbaekdahl@pm.me>
2020-04-15 11:47:08 +0200

    Update dependencies; run black ajnd isort

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
 
-with open('./README.rst', encoding='utf-8') as readme:
+with open("./README.rst", encoding="utf-8") as readme:
     long_description = readme.read()
 
 setup(
-    name='sentiment_lexicon',
-    version='0.2.3',
+    name="sentiment_lexicon",
+    version="0.2.3",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    packages=['sentiment_lexicon'],
-    url='https://github.com/emilbaekdahl/sentiment_lexicon',
-    author='Emil Bækdahl',
+    long_description_content_type="text/x-rst",
+    packages=["sentiment_lexicon"],
+    url="https://github.com/emilbaekdahl/sentiment_lexicon",
+    author="Emil Bækdahl",
     author_email="emilsbaekdahl@gmail.com",
-    project_urls={'Source Code': 'https://github.com/emilbaekdahl/sentiment_lexicon',
-                  'Documentation': 'https://emilbaekdahl.github.io/sentiment_lexicon/'},
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Developers', 'License :: OSI Approved :: MIT License']
+    project_urls={
+        "Source Code": "https://github.com/emilbaekdahl/sentiment_lexicon",
+        "Documentation": "https://emilbaekdahl.github.io/sentiment_lexicon/",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
